b1bf7a1dec72037bf53dd48fb2422a91846f831c
Rahul Agarwal <imagarwal05@gmail.com>
2025-05-07 12:34:34 +0530

    Modified UI in for recent orders and added lucide-react library.

--- app/dashboard/Table/Table.tsx
+++ app/dashboard/Table/Table.tsx
@@ -52,7 +52,7 @@ const tableData = [
 
 const Table = () => {
     return (
-        <table className={styles.table}>
+        <table className={`${styles.table} p-4 bg-white shadow-sm`}>
             <thead className="bg-gray-300">
                 <tr>
                     <th className="font-normal">ORDER ID</th>
@@ -62,24 +62,24 @@ const Table = () => {
                     <th className="font-normal">DUE DATE</th>
                     <th className="font-normal">REQUEST STATUS</th>
                     <th className="font-normal">AMOUNT</th>
-                    <th className="font-normal">ACTIONS</th>
+                    <th className="font-normal text-center">ACTIONS</th>
                 </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-                {tableData.map((data, index) => (
-                    <tr className="hover:bg-gray-50" key={index}>
-                        <td className="text-sm text-center">{data.orderNo}</td>
-                        <td className="text-sm text-center">
-                            <div className="flex items-center justify-center gap-2">
+                {tableData.map((data, idx) => (
+                    <tr key={idx} className="hover:bg-gray-50">
+                        <td className="text-sm">{data.orderNo}</td>
+                        <td className="text-sm">
+                            <div className="flex items-center gap-2">
                                 <User />
-                                <div className="flex flex-col items-start">
+                                <div className="flex flex-col">
                                     <span>{data.customer.name}</span>
                                     <span>{data.customer.email}</span>
                                 </div>
                             </div>
                         </td>
-                        <td className="text-sm text-center">{data.type}</td>
-                        <td className="text-sm text-center">
+                        <td className="text-sm">{data.type}</td>
+                        <td className="text-sm">
                             {data.status === 0 ? (
                                 <span className="px-4 inline-flex text-xs leading-5 font-semibold rounded-full bg-[#007AFF26] text-[#007AFF]">
                                     New
@@ -98,32 +98,32 @@ const Table = () => {
                                 </span>
                             )}
                         </td>
-                        <td className="text-sm text-center">{data.dueDate}</td>
-                        <td className="text-sm text-center">
-                            <div className="flex gap-4 justify-center items-center">
-                                {data.requestStatus === undefined ? (
+                        <td className="text-sm">{data.dueDate}</td>
+                        <td className="text-sm">
+                            <div className="flex gap-4">
+                                {data.requestStatus == null ? (
                                     <>
-                                        <button className="bg-[#34C759]/15 border border-[#34C759] rounded-4xl px-6 text-[#34C759]">
+                                        <button className="border border-[#34C759] rounded-4xl w-24 text-[#34C759] bg-[#34C759]/15">
                                             Accept
                                         </button>
-                                        <button className="bg-[#FF3B3026]/15 border border-[#FF2D55] rounded-4xl px-6 text-[#FF3B30]">
+                                        <button className="border border-[#FF2D55] rounded-4xl w-24 text-[#FF3B30] bg-[#FF3B30]/15">
                                             Deny
                                         </button>
                                     </>
-                                ) : data.requestStatus === true ? (
-                                    <button className="bg-[#34C759]/15 border border-[#34C759] rounded-4xl px-6 text-[#34C759]">
+                                ) : data.requestStatus ? (
+                                    <button className="border border-[#34C759] rounded-4xl w-24 text-[#34C759] bg-[#34C759]/15">
                                         Accepted
                                     </button>
                                 ) : (
-                                    <button className="bg-[#FF3B3026]/15 border border-[#FF2D55] rounded-4xl px-6 text-[#FF3B30]">
+                                    <button className="border border-[#FF2D55] rounded-4xl w-24 text-[#FF3B30] bg-[#FF3B30]/15">
                                         Denied
                                     </button>
                                 )}
                             </div>
                         </td>
-                        <td className="text-sm text-center">₹{data.amount}.00</td>
+                        <td className="text-sm">₹{data.amount}.00</td>
                         <td className="text-center">
-                            <div className="flex items-center justify-center gap-3">
+                            <div className="flex justify-center gap-3">
                                 <button>
                                     <PenBoxIcon className="w-5 h-5" />
                                 </button>
