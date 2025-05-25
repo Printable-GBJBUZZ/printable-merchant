b2e00cbab635ac29c66a64b47db2ad1bfea6feba
piyushcodes07 <piyushcodes07@gmail.com>
2025-05-25 21:30:50 +0530

    changed styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
   SignedIn,
-  SignedOut,
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
