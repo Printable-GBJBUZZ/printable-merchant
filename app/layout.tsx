08e68fa2e03c70904dd3b2e6787c53465b58cc93
Rahul Agarwal <imagarwal05@gmail.com>
2025-06-18 12:13:34 +0530

    Change in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 
 					<div className="flex-1 flex flex-col">
 						<Header />
-						<main className="flex-1 overflow-auto mt-4">{children}</main>
+						<main className="flex-1 overflow-auto mt-4 flex flex-col">{children}</main>
 					</div>
 				</div>
 			</body>
